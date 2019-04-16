6b0742a04500ec9cf6a3e0813798ba032cbdbce8
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-16 13:50:01 +0200

    Implement method: loadXMLDocument to load .xml file

--- src/main/java/com/codecool/dao/CardDaoXML.java
+++ src/main/java/com/codecool/dao/CardDaoXML.java
@@ -1,4 +1,36 @@
 package com.codecool.dao;
 
-public class CardDaoXML {
+import com.codecool.gameelement.Card;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardDaoXML implements CardDao {
+    private List<Card> allCards = new ArrayList<>();
+    private Document doc;
+
+    @Override
+    public List<Card> getAllCards() {
+        return allCards;
+    }
+
+    private void loadXMLDocument(String xmlPath) {
+        Document doc;
+        try {
+            File fileToRead = new File(xmlPath);
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            doc = documentBuilder.parse(fileToRead);
+            doc.getDocumentElement().normalize();
+        } catch (ParserConfigurationException | IOException | SAXException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
