10397c8b5eb211f8456ccac4cc1ae5ad200925c4
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-16 14:54:22 +0200

    Move loadXMLDocumnet to CardDaoLoadXML abstract class

--- src/main/java/com/codecool/dao/CardDaoXML.java
+++ src/main/java/com/codecool/dao/CardDaoXML.java
@@ -2,34 +2,27 @@ package com.codecool.dao;
 
 import com.codecool.gameelement.Card;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CardDaoXML implements CardDao {
+public class CardDaoXML extends CardDaoLoadXML implements CardDao {
     private List<Card> allCards = new ArrayList<>();
-    private Document doc;
+
+    public CardDaoXML() {
+        loadXMLDocument("src/main/resources/cards.xml");
+        parseCards(getDoc());
+    }
+
+    private void parseCards(Document document) {
+
+    }
+
 
     @Override
     public List<Card> getAllCards() {
         return allCards;
     }
 
-    private void loadXMLDocument(String xmlPath) {
-        try {
-            File fileToRead = new File(xmlPath);
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            doc = documentBuilder.parse(fileToRead);
-            doc.getDocumentElement().normalize();
-        } catch (ParserConfigurationException | IOException | SAXException ex) {
-            ex.printStackTrace();
-        }
-    }
+
 }
