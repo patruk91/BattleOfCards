83f320c70ad784cc053c50bbe6232727df758ee5
Pawel <p.j.bieniek@wp.pl>
2019-04-17 10:38:15 +0200

    TopSpeedComparator class implemented

--- src/main/java/com/codecool/comparator/TopSpeedComparator.java
+++ src/main/java/com/codecool/comparator/TopSpeedComparator.java
@@ -1,4 +1,19 @@
 package com.codecool.comparator;
 
-public class TopSpeedComparator {
+import com.codecool.gameelement.Card;
+
+import java.util.Comparator;
+
+public class TopSpeedComparator implements Comparator<Card> {
+    @Override
+    public int compare(Card myCard, Card opponentCard) {
+        double result = myCard.getTopSpeed() - opponentCard.getTopSpeed();
+        if (result > 0) {
+            return 1;
+        } else if (result < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
