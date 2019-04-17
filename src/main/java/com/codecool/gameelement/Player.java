9f2898a489246bbc81aeb02f01915599692aac8d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-17 07:56:26 +0200

    Add methods, in Player class: getters, setters

--- src/main/java/com/codecool/gameelement/Player.java
+++ src/main/java/com/codecool/gameelement/Player.java
@@ -1,4 +1,27 @@
 package com.codecool.gameelement;
 
 public class Player {
+    private String playerName;
+    private Pile pile;
+    private boolean isPlayerFirst = false;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public Pile getPile() {
+        return pile;
+    }
+
+    public boolean isPlayerFirst() {
+        return isPlayerFirst;
+    }
+
+    public void setPlayerFirst(boolean isPlayerFirst) {
+        this.isPlayerFirst = isPlayerFirst;
+    }
 }
