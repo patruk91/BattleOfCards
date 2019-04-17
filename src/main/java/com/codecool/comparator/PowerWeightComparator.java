4fdba00649d625ab9d31d7f2aa2d65c235f05e32
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-17 09:38:05 +0200

    Override method compare in PowerWeigthComparator

--- src/main/java/com/codecool/comparator/PowerWeightComparator.java
+++ src/main/java/com/codecool/comparator/PowerWeightComparator.java
@@ -1,4 +1,19 @@
 package com.codecool.comparator;
 
-public class PowerWeightComparator {
+import com.codecool.gameelement.Card;
+
+import java.util.Comparator;
+
+public class PowerWeightComparator implements Comparator<Card> {
+    @Override
+    public int compare(Card myCard, Card opponentCard) {
+        double result = myCard.getPowerWeightRatio() - opponentCard.getPowerWeightRatio();
+        if (result > 0) {
+            return 1;
+        } else if (result < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
