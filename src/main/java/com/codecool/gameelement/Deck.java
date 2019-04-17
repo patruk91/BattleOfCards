40e11b6277ebe351d410071cd9088b19c87198fe
Krzysiek <krzysiekczaja@gmail.com>
2019-04-17 09:19:51 +0200

    Add all methods of Deck class

--- src/main/java/com/codecool/gameelement/Deck.java
+++ src/main/java/com/codecool/gameelement/Deck.java
@@ -1,4 +1,20 @@
 package com.codecool.gameelement;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+    List<Card> cards;
+
+    public Deck(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public List<Card> getDeck() {
+        return cards;
+    }
+
+    public void shuffleDeck(Deck deck) {
+        Collections.shuffle(deck.getDeck());
+    }
 }
