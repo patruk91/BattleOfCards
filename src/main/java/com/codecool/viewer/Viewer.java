1aedd30a97fbff68ed273b7d0e2ea53b35c004dc
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-04-18 13:07:51 +0200

    Add clearScreen method

--- src/main/java/com/codecool/viewer/Viewer.java
+++ src/main/java/com/codecool/viewer/Viewer.java
@@ -40,4 +40,9 @@ public class Viewer {
     public void credits() {
         System.out.println("Credits");
     }
+
+    public void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
